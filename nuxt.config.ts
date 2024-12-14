a96659b3b745b5648a99af5322dfcd163902affb
Phuongp2003 <phambuinamhphuong@gmail.com>
2024-12-15 01:23:01 +0700

    add studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
-	modules: ['@vueuse/nuxt'],
+	modules: ['@vueuse/nuxt', '@nuxthq/studio'],
 	css: ['@/assets/css/tailwind.css'],
 	postcss: {
 		plugins: {
